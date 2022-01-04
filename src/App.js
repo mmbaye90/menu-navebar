27f8599d2a7aa930805e5464593a8ed337349ee6
Mbaye <mmbaye1@hotmail.fr>
2022-01-04 15:00:02 +0100

    Ajout des icones

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import * as Icons from "react-icons/fa";
 function App() {
   return (
     <div className="App">
-      <h1>
-        <Icons.FaPlus />
-      </h1>{" "}
+      <Icons.FaPlus />
+      <Icons.FaBell />
+      <Icons.FaFacebookMessenger />
+      <Icons.FaArrowDown />
     </div>
   );
 }
