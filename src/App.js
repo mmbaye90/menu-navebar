dc7e496427e5547b7304d266c40095c1232d8f01
Mbaye <mmbaye1@hotmail.fr>
2022-01-04 17:48:52 +0100

    Création de la fonction pour la gestion du click et son affichage

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import "./App.css";
 import { CSSTransition } from "react-transition-group";
 import * as Icons from "react-icons/fa";
+import { useState } from "react";
 
 function App() {
   return (
     <div className="App">
       <Navbar>
-        <Icons.FaPlus />
-        <Icons.FaBell />
-        <Icons.FaFacebookMessenger />
-        <Icons.FaArrowDown />
+        <NavItem icon={<Icons.FaPlus />} />
+        <NavItem icon={<Icons.FaBell />} />
+        <NavItem icon={<Icons.FaFacebookMessenger />} />
+
+        {/* Affichage de la fonction DropDown au clic de cet élément */}
+        <NavItem icon={<Icons.FaArrowDown />}>{/* <DropDownItem/> */}</NavItem>
       </Navbar>
     </div>
   );
 }
 
+//Création de la function navbar pour afficher et englober les autres fonctions
 function Navbar(props) {
   return (
     <nav>
@@ -23,4 +27,23 @@ function Navbar(props) {
   );
 }
 
+//Création de la fonction NavItem pour la gestion de l'affichage au clic du dernier élément
+
+function NavItem({ icon, children }) {
+  /*J'initialise mon state pour gerer l'événement au clic et je le fait démarrer à false c a d rien ne s'affiche avant l'év du click
+  Au lieu de mettre le setOpen à true,je le met à !different de open,ceci me permettra d'afficher ou de masquer au click*/
+
+  const [open, setOpen] = useState(false);
+
+  return (
+    //J'initialise un evenement de type Click sur l'élément du a
+
+    <li>
+      <a href="#" onClick={() => setOpen(!open)}>
+        {icon}
+      </a>
+      {open && console.log("yes")}
+    </li>
+  );
+}
 export default App;
